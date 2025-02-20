b68243fdd8dd500ec9e9299079089827f55939c7
Deepak Porwal <132838666+Deepakporwal01@users.noreply.github.com>
2025-02-20 22:15:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 import { router } from "./routes/index.js";
 const app = express();
 app.use(cors({
-    origin:"*",  
+    origin:"https://shopsphere-vert.vercel.app",  
     credentials:true, 
     
 })); 
